20d1b304a79ad5aa8e282708d75cb3d68f3e592b
subodhss23@gmail.com <subodhss23@gmail.com>
2019-08-27 19:23:22 -0700

    login and logout

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -2,34 +2,74 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function UserGreeting(props){
-  return <h1>Welcome back!</h1>
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
 }
 
-function GuestGreeting(props){
-  return <h1>Please sign up.</h1>
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
 }
 
-function Greeting(props){
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
   const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn){
+  if (isLoggedIn) {
     return <UserGreeting />;
   }
   return <GuestGreeting />;
 }
 
-function LoginButton(props){
+function LoginButton(props) {
   return (
-    <button onClick={props.onClick}>Login</button>
+    <button onClick={props.onClick}>
+      Login
+    </button>
   );
 }
 
-function LogoutButton(props){
+function LogoutButton(props) {
   return (
-    <button onClick={props.onClick}>Logout</button>
+    <button onClick={props.onClick}>
+      Logout
+    </button>
   );
 }
 
 ReactDOM.render(
-  <Greeting isLoggedIn={true} />, document.getElementById('root')
+  <LoginControl />,
+  document.getElementById('root')
 );
