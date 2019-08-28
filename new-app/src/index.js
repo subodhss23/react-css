7df65de051dee45e9ba3a61d838a32a38f6398b2
subodhss23@gmail.com <subodhss23@gmail.com>
2019-08-27 20:05:23 -0700

    changes

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -1,78 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class LoginControl extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      isLoggedIn : false
-    }
 
-    this.handleLoggedInClick = this.handleLoggedInClick.bind(this);
-    this.handleLoggedOutClick = this.handleLoggedOutClick.bind(this);
-  }
-
-  handleLoggedInClick(){
-    this.setState({
-      isLoggedIn : true
-    })
-  }
-
-  handleLoggedOutClick(){
-    this.setState({
-      isLoggedIn: false
-    })
-  }
-
-  render(){
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-
-       if(isLoggedIn){
-       button = <LogoutButton onClick={this.handleLoggedOutClick} />; 
-      } else {
-         button = <LoginButton onClick={this.handleLoggedInClick} />;
-       }
-       
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    );
-  }
-}
-
-function UserGreeting(props){
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props){
-  return <h1>Please sign up.</h1>
-}
-
-function Greeting(props){
-  const isLoggedIn = props.isLoggedIn;
-  if(isLoggedIn){
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />
-}
-
-function LoginButton(props){
+function Mailbox(props){
+  const unreadMessage = props.unreadMessage;
   return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessage.length > 0 &&
+        <h2>
+          You have {unreadMessage.length} unread message.
+        </h2>}
+    </div>
   );
 }
 
-function LogoutButton(props){
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  )
-}
-
-ReactDOM.render(<LoginControl />,document.getElementById('root'));
+const message = ['React', 'Re: React', Re:Re:React];
+ReactDOM.render(
+  <Mainbox unreadMessage={messages} />,
+  document.getElementById('root')
+);
